391df74b31a3effea5edf426a1aaa255f6cbea06
Bradley <bradley@appenate.com>
2021-06-25 15:17:37 +1000

    Mod

--- backup.xml/Android.mk
+++ backup.xml/Android.mk
@@ -129,15 +129,6 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := preinstalled-packages-platform-handheld-product.xml
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/sysconfig
-LOCAL_OVERRIDES_PACKAGES := preinstalled-packages-platform-handheld-product.xml
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := preinstalled-packages-platform-overlays.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
