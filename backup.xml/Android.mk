21aad08b9c3530f7a934a47abe151959f32a2897
Bradley <bradley@appenate.com>
2021-06-27 08:51:00 +1000

    Revert "mod"
    This reverts commit c6d215bea174c832a73fd60de840392e650d2800.

--- backup.xml/Android.mk
+++ backup.xml/Android.mk
@@ -129,6 +129,23 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := preinstalled-packages-platform-handheld-product.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/sysconfig
+LOCAL_OVERRIDES_PACKAGES := preinstalled-packages-platform-handheld-product.xml
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := preinstalled-packages-platform-overlays.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/sysconfig
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := wellbeing.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
@@ -143,5 +160,5 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/sysconfig
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_REQUIRED_MODULES := dialer_experience.xml google-hiddenapi-package-whitelist.xml google-rollback-package-whitelist.xml google-staged-installer-whitelist.xml google_build.xml google_vr_build.xml nexus.xml nga.xml pixel.xml pixel_2019_exclusive.xml pixel_experience_2017.xml pixel_experience_2018.xml pixel_experience_2019.xml pixel_experience_2019_midyear.xml pixel_experience_2020.xml pixel_experience_2020_midyear.xml wellbeing.xml
+LOCAL_REQUIRED_MODULES := dialer_experience.xml google-hiddenapi-package-whitelist.xml google-rollback-package-whitelist.xml google-staged-installer-whitelist.xml google_build.xml google_vr_build.xml nexus.xml nga.xml pixel.xml pixel_2019_exclusive.xml pixel_experience_2017.xml pixel_experience_2018.xml pixel_experience_2019.xml pixel_experience_2019_midyear.xml pixel_experience_2020.xml pixel_experience_2020_midyear.xml preinstalled-packages-platform-handheld-product.xml preinstalled-packages-platform-overlays.xml wellbeing.xml
 include $(BUILD_PREBUILT)
