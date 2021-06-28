7e1b66e514292f4bbf6a598e1bc64b79a1e7bd74
Bradley <bradley@appenate.com>
2021-06-28 18:37:59 +1000

    Change Signing

--- LibertyServices/Android.mk
+++ LibertyServices/Android.mk
@@ -15,7 +15,7 @@ LOCAL_SRC_FILES := com.libertyphoneproject.services.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_CERTIFICATE := platform
 LOCAL_REQUIRED_MODULES := privapp-permissions-com.libertyphoneproject.services.xml
 include $(BUILD_PREBUILT)
 
